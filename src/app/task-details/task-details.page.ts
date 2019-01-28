e710200e63cbed1f9baade6d011f30c0798a9fa0
OfflineProgrammer <mmalaka@gmail.com>
2019-01-27 16:33:54 -0800

    back button

--- src/app/task-details/task-details.page.ts
+++ src/app/task-details/task-details.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Task} from '../classes/task'
 import { KidsService } from "../services/kids.service";
 import { ActivatedRoute } from "@angular/router";
+import {  NavController } from "@ionic/angular";
 
 @Component({
   selector: 'app-task-details',
@@ -16,12 +17,17 @@ export class TaskDetailsPage implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private kidsService: KidsService,
+    private navCtrl: NavController
   ) { 
     this.task ={
       id:'',
       name:'',
       bComplete:false
     }
+  }
+
+  goBack() {
+    this.navCtrl.navigateBack('taskz/'+this.kid_Id+'/'+this.plan_Id);
   }
 
   ngOnInit() {
